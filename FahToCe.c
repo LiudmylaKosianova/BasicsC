4867f04c73467dda98796be9b9a93ee9eb341906
snowwhitesheep <snw.wht.lk@gmail.com>
2023-07-23 14:44:17 +0200

    start

---
+++ FahToCe.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    double fahrenheit;
+    scanf("%lf", &fahrenheit);
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    printf("%lf", celsius);
+    return 0;
+}
