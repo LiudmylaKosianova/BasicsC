fb6f69603e146c153c9bf3118539015e6ddc6bb1
snowwhitesheep <snw.wht.lk@gmail.com>
2023-07-26 12:35:20 +0200

    start

---
+++ ArmstrongOrNot.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//checks if a number is an armstrong number
+//(armstrong number = sum of cubes of every digit equals the number)
+
+int main (){
+    int number;
+    scanf("%d", &number);
+    int originalNumber = number;
+    int sum, remainder;
+
+    while(originalNumber){
+
+    }
+    return 0;
+}
