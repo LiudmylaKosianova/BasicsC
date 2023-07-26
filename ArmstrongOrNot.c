cf7bc6735e0ddac299c199344d05b487a39a4e2b
snowwhitesheep <snw.wht.lk@gmail.com>
2023-07-26 13:14:28 +0200

    done

--- ArmstrongOrNot.c
+++ ArmstrongOrNot.c
@@ -6,10 +6,17 @@ int main (){
     int number;
     scanf("%d", &number);
     int originalNumber = number;
-    int sum, remainder;
+    int sum, remainder = 0;
 
-    while(originalNumber){
-
+    while(originalNumber != 0){
+        remainder = originalNumber%10;        
+        sum += remainder*remainder*remainder;
+        originalNumber = originalNumber/10;
+    }
+    if(sum == number){
+        printf("An Armstrong number");
+    }else{
+        printf("Not an Armstrong");
     }
     return 0;
 }
